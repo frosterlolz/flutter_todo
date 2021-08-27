28d82bec2474aa71ba3981d5b7bc02ccd3c345b2
frosterlolz <62335827+frosterlolz@users.noreply.github.com>
2021-08-27 11:25:34 +0300

    Now I can hide tabbar on scrolling

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,6 +11,7 @@ class _HomeState extends State<Home> {
 
   String? _userToDo;
   List todoList = [];
+  List doneList = [];
 
 
   @override
@@ -19,23 +20,26 @@ class _HomeState extends State<Home> {
     super.initState();
 
     todoList.addAll(['Wake up', 'Wash dishes', 'Buy milk', 'Go for walk', 'Buy water', 'русское дело', 'Погулять', 'Доделать дизайн приложения', 'Cоздать аккаут в Apple']);
+    todoList.addAll(['Wake up', 'Wash dishes', 'Buy milk', 'Go for walk', 'Buy water', 'русское дело', 'Погулять', 'Доделать дизайн приложения', 'Cоздать аккаут в Apple']);
+    todoList.addAll(['Wake up', 'Wash dishes', 'Buy milk', 'Go for walk', 'Buy water', 'русское дело', 'Погулять', 'Доделать дизайн приложения', 'Cоздать аккаут в Apple']);
   }
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(length: 2, 
         child: Scaffold(
-          appBar: AppBar(
-            flexibleSpace: Column(
+          body: NestedScrollView(
+              floatHeaderSlivers: true,
+              headerSliverBuilder: (context, innerBoxIsScrolled) => [
+            SliverAppBar(
+              floating: true,
+              snap: true,
+              flexibleSpace: Column(
               mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TabBar(tabs: [
-                  Tab(text: 'Новые',),
-                  Tab(text: 'Выполненные',)
-                ])
-              ],
+                children: [TabBar(tabs: [Tab(text: 'Новые',), Tab(text: 'Выполненные',)])],
+              ),
             ),
-          ),
+          ],
           body: TabBarView(
             children: [
               Scaffold( // возвращает некий слой
@@ -51,85 +55,88 @@ class _HomeState extends State<Home> {
                       title: Text(todoList[index]),
                       trailing: IconButton( // добавляем иконку-кнопку, чтобы удалять не только свайпом
                         icon: Icon(
-                          Icons.delete_sweep,
-                          color: Colors.grey,
+                          Icons.done_outline_rounded,
+                          color: Colors.green,
                         ),
-                        onPressed: () {setState(() {todoList.removeAt(index);});}, // действие аналогичное, но уже при нажатии на кнопку
+                        onPressed: () {setState(() {
+                          doneList.add(todoList[index]);
+                          todoList.removeAt(index);
+                        }
+                        );
+                        }, // действие аналогичное, но уже при нажатии на кнопку
                       ),
                     ),
                   ),
                   direction: DismissDirection.endToStart,
                   onDismissed: (direction) // обрабатываем, что произойдет с объектом при свайпе
-                  {setState(() {todoList.removeAt(index);});},
+                  {setState(() {
+                    doneList.add(todoList[index]);
+                    todoList.removeAt(index);});},
                 );
               }
           ),
-          floatingActionButton: FloatingActionButton( // добавляем кнопку + добавлять записи в массив
-            backgroundColor: Colors.lightBlue[700],
-            onPressed: () {
-              showDialog(context: context, builder: (BuildContext context){ // при нажатии создается поле showDialog, в котором:
-                return AlertDialog( // сам элемент AlertDialog
-                  title: Text('Добавить элемент'),
-                  content: TextField(
-                    onChanged: (String value) { // при вводе информации мы помещаем её в поле userToDo
-                      _userToDo = value;
-                    },
-                  ),
-                  actions: [ // добавляем кнопки(у) всплывающему окну
-                    ElevatedButton(onPressed: (){
-                      setState(() {
-                        todoList.add(_userToDo); // кнопка используя состояние добавляет запись из userToDo в массив todoList
-                      });
+                  floatingActionButton: FloatingActionButton( // добавляем кнопку + добавлять записи в массив
+                  backgroundColor: Colors.lightBlue[700],
+                  onPressed: () {
+                    showDialog(context: context, builder: (BuildContext context){ // при нажатии создается поле showDialog, в котором:
+                      return AlertDialog( // сам элемент AlertDialog
+                        title: Text('Добавить элемент'),
+                        content: TextField(
+                          onChanged: (String value) { // при вводе информации мы помещаем её в поле userToDo
+                            _userToDo = value;
+                          },
+                        ),
+                        actions: [ // добавляем кнопки(у) всплывающему окну
+                          ElevatedButton(onPressed: (){
+                            setState(() {
+                              todoList.add(_userToDo); // кнопка используя состояние добавляет запись из userToDo в массив todoList
+                            });
 
-                      Navigator.of(context).pop(); // закрывает всплывающие окна
-                    }, child: Text('Добавить'))
-                  ],
-                );
-              }); // context - страничка, на которой мы находимся
-            },
-            child: Icon(
-              Icons.add_sharp,
-              size: 25,
-              color: Colors.white,
-            ),
-          ),
-        ),
-              Icon(Icons.home)
-            ],
-          ),
-        ),
+                            Navigator.of(context).pop(); // закрывает всплывающие окна
+                          }, child: Text('Добавить'))
+                        ],
+                      );
+                    }); // context - страничка, на которой мы находимся
+                  },
+                  child: Icon(
+                    Icons.add_sharp,
+                    size: 25,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              // НАЧИНАЕТСЯ БЛОК "ВЫПОЛНЕННЫЕ"
+              Scaffold( // возвращает некий слой
+                backgroundColor: Colors.grey[300],
+                body: ListView.builder( // ListView позволяет создавать объекты в формате списка
+                    itemCount: doneList.length, // длина массива/списка
+                    itemBuilder: (BuildContext context, int index) // перебирает полностью весь список
+                    {
+                      return Dismissible( // формирует блок, в котором будут выводиться элементы массива todo_list, который можно удалить свайпом
+                        key: Key(doneList[index]), // берем элемент списка по ключу
+                        child: Card( // в теле блока выводим карочку (некий блок, в котором мы можем вывести информацию)
+                          child: ListTile( // которая содержит ListTile (есть также ListBody) с текстом элемента массива, взятого по ключу
+                            title: Text(doneList[index]),
+                            trailing: IconButton( // добавляем иконку-кнопку, чтобы удалять не только свайпом
+                              icon: Icon(
+                                Icons.delete_sweep,
+                                color: Colors.grey,
+                              ),
+                              onPressed: () {setState(() {doneList.removeAt(index);});}, // действие аналогичное, но уже при нажатии на кнопку
+                            ),
+                          ),
+                        ),
+                        direction: DismissDirection.endToStart,
+                        onDismissed: (direction) // обрабатываем, что произойдет с объектом при свайпе
+                        {setState(() {doneList.removeAt(index);});},
+                      );
+                    }
+                ),
+              ),
+      ],
+    ),
+    ),
+    ),
     );
   }
 }
-
-
-
-
-
-
-// void _menuOpen(){ // реализуем выезжающее меню
-//   Navigator.of(context).push(
-//     MaterialPageRoute(builder: (BuildContext context) {
-//       return Scaffold(
-//           appBar: AppBar(title: Text('Menu'),),
-//           body: Row(
-//             children: [
-//               ElevatedButton(onPressed: ()
-//               {
-//                 Navigator.pop(context);
-//                 Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
-//               },
-//                   child: Text('На главную')),
-//             ElevatedButton(onPressed: (){}, child: Text('пустышка')),
-//             ElevatedButton(onPressed: (){
-//               Navigator.pop(context);
-//               Navigator.pushReplacementNamed(context, '/profile');
-//             }, child: Text('Профиль')),
-//             // можно добавить еще кнопок
-//             Padding(padding: EdgeInsets.only(left:15)),
-//           ],
-//         )
-//       );
-//     })
-//   );
-// }
--- lib/pages/test.dart
+++ lib/pages/test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo/pages/home.dart';
 
 class Test extends StatefulWidget {
   const Test({Key? key}) : super(key: key);
@@ -7,34 +8,60 @@ class Test extends StatefulWidget {
   _TestState createState() => _TestState();
 }
 
-class _TestState extends State<Test> {
+class _TestState extends State<Test>  with SingleTickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-        appBar: AppBar(
-          flexibleSpace: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              TabBar(tabs: [
-                Tab(
-                  text: 'Новые',
-                ),
-                Tab(
-                  text: 'Выполненные',
-                )
-              ])
-            ],
-          ),
-        ),
+
+        body: NestedScrollView(
+          floatHeaderSlivers: true,
+          headerSliverBuilder: (context, innerBoxIsScrolled) => [
+            SliverAppBar(
+              floating: true,
+              flexibleSpace: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [TabBar(tabs: [Tab(text: 'Новые',), Tab(text: 'Выполненные',)])],
+              ),
+            ),
+          ],
+
+
         body: TabBarView(
           children: [
-            Icon(Icons.home),
+            Home(),
             Icon(Icons.error_outline)
           ],
         ),
-      ),);
+        ),
+      ),
+    );
   }
 }
+//     return DefaultTabController(
+//       length: 2,
+//       child: Scaffold(
+//         appBar: AppBar(
+//           flexibleSpace: Column(
+//             mainAxisAlignment: MainAxisAlignment.end,
+//             children: [TabBar(tabs: [Tab(text: 'Новые',), Tab(text: 'Выполненные',)])],
+//           ),
+//         ),
+//         body: TabBarView(
+//           children: [
+//             Icon(Icons.home),
+//             Icon(Icons.error_outline)
+//           ],
+//         ),
+//       ),);
+
 
